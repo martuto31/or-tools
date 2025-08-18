68ec3a3e9f06c22cdc4d84a6cd8d1f75d0240788
Martin Petrov <petrovmarti21@gmail.com>
2025-08-18 03:20:16 +0300

    Add ORService service

--- or-tools/Program.cs
+++ or-tools/Program.cs
@@ -1,9 +1,12 @@
+using or_tools.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddScoped<ORService>();
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
